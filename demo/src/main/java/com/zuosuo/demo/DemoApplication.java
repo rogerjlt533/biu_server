481a1d9e1abda998ba238f60b660b2381ef76c6b
fallen <rogerk08@126.com>
2021-10-19 17:57:43 +0800

    * demo

--- demo/src/main/java/com/zuosuo/demo/DemoApplication.java
+++ demo/src/main/java/com/zuosuo/demo/DemoApplication.java
@@ -1,9 +1,16 @@
 package com.zuosuo.demo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@ComponentScan(basePackages = {"com.zuosuo.component", "com.zuosuo.wechat", "com.zuosuo.mybatis", "com.zuosuo.cache", "com.zuosuo.biudb", "com.zuosuo.auth", "com.zuosuo.demo"})
+@MapperScan(basePackages = {"com.zuosuo.biudb.mapper"})
+@EntityScan(basePackages = {"com.zuosuo.biudb.entity"})
 public class DemoApplication {
 
     public static void main(String[] args) {
